e2ca074377b363a40b50eaa7177d67ce5c9aa83a
batilda <mariagofmanv@gmail.com>
2018-02-11 20:12:07 +0500

    first stage ended
    done gallery and bottom

---
+++ js/script.js
@@ -0,0 +1,9 @@
+ //if (document.body.gallery.background) {
+// 	alert('You clicked on my first picture');
+ //}
+
+alert('test');
+
+//document.body.headertop.onClick = alert('You clicked on my first picture');
+
+//document.body.gallery.gallery-images.gallery-href.gallery-image-1.onClick = alert('You clicked on my first picture');
