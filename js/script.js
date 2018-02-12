461c746cc93013d67b21e0d2bb699d2a391c962f
batilda <mariagofmanv@gmail.com>
2018-02-12 23:49:01 +0500

    gallery outlines
    gallery outlines added

--- js/script.js
+++ js/script.js
@@ -2,8 +2,25 @@
 // 	alert('You clicked on my first picture');
  //}
 
-alert('test');
+function galleryImage1 () {
+	alert('Cheese Sticks - on mondays!');
+}
+
+function galleryImage2 () {
+	alert('Croissants - on tuesdays!');
+}
+
+function galleryImage4 () {
+	alert('Pizza - on wednesdays!');
+}
+
+function galleryImage3 () {
+	alert('Berries - on thursdays!');
+}
+
+function galleryImage5() {
+	alert('Noodle - on fridays!');
+}
 
 //document.body.headertop.onClick = alert('You clicked on my first picture');
-
 //document.body.gallery.gallery-images.gallery-href.gallery-image-1.onClick = alert('You clicked on my first picture');
